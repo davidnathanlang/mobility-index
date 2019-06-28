ba661f19d2b539e1e9c60c8c44f0560182941480
David Lang <dnlang86@stanford.edu>
2019-06-28 10:17:41 -0700

    add test_gpd_tools

--- mobility_index/gdb_tools.py
+++ mobility_index/gdb_tools.py
@@ -1,11 +1,11 @@
 def pandas2geopandas(df):
     # a function to convert a pandas data frame to geopandas data frame
-<<<<<<< HEAD
+
     
 
 def convex_hull_shape(gf):
     # calculate convex shape
-=======
+
     # ...
     # returns a geopandas dataframe
     return(gf)
@@ -15,4 +15,4 @@ def convex_hull_shape(gf):
     # ...
     # returns geopandas dataframe with the polygon shape
     return(gfShape)
->>>>>>> 1505a3d73f89a1d02ef1382742e4b747bbd302d4
+
